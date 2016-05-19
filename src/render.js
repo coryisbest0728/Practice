c8ee9f5c891d9449e0a8184dee0fa830e86eccf3
kuanghongrui <kuanghongrui@baijiahulian.com>
2016-05-19 20:30:03 +0800

    add render example

--- src/render.js
+++ src/render.js
@@ -8,9 +8,30 @@ define(function() {
     bs.backgroundColor = '#CCC';
 
     bs.fontSize = '14px';
-
     document.body.appendChild(document.createTextNode('abc!'));
 
     bs.color = 'green';
     bs.backgroundColor = '#FFF000';
 });
+
+
+
+
+// timeShim([
+//     function () {
+//         bs.padding = '2px';
+//         bs.border = '1px solid red';
+//     },
+//     function () {
+//         bs.color = 'blue';
+//         bs.backgroundColor = '#CCC';
+//     },
+//     function () {
+//         bs.fontSize = '14px';
+//         document.body.appendChild(document.createTextNode('abc!'));
+//     },
+//     function () {
+//         bs.color = 'green';
+//         bs.backgroundColor = '#FFF000';
+//     }
+// ]);
